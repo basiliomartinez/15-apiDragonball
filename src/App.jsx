7271ce60af91b397d94c6e9e6f755dab86a35359
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-11-10 10:08:09 -0300

    Maquetado del logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import logo from "./assets/Logo_Dragon_Ball_Z.jpg"
 function App() {
 
   return (
     <>
+    <main className="container my-5 text-center">
+    <img src={logo} alt="logo dragon ball Z" />
     <h1>Hola mundo</h1>
+    </main>
     </>
+    
   )
 }
 
