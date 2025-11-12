db6feadda2cc95053854b43bc57d7897e1d02ac4
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-11-12 07:12:02 -0300

    Mostramos y ocultamos el componente spinner cuando se esta haciendo la solicitud a la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
 import logo from "./assets/Logo_Dragon_Ball_Z.jpg";
 import CardPersonaje from "./componentes/CardPersonaje";
-import { Button } from "react-bootstrap";
-
+import { Button, Spinner } from "react-bootstrap";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState (true)
 
   useEffect(() => {
     obtenerPersonaje();
   }, []);
 
+
   const obtenerPersonaje = async () => {
     try {
+      setMostrarSpinner(true)
+      const numeroAleatorio = Math.floor(Math.random() * 58) + 1;
       const response = await fetch(
-        "https://dragonball-api.com/api/characters/1"
+        `https://dragonball-api.com/api/characters/ ${numeroAleatorio}`
       );
       console.log(response);
-      if(response.status===200){
-        const datos=  await response.json()
-        console.log(datos)
-        setPersonaje(datos)
+      if (response.status === 200) {
+        const datos = await response.json();
+        console.log(datos);
+        setPersonaje(datos);
+setMostrarSpinner(false)
+      } else if (response.status === 400) {
+        alert("No se pudo obetener los datos del personaje");
       }
     } catch (error) {
       console.error(error);
@@ -29,10 +35,19 @@ function App() {
 
   return (
     <>
-      <main className="container my-5 text-center">
-        <img src={logo} alt="logo dragon ball Z" />
-        <CardPersonaje personaje= {personaje}></CardPersonaje>
-        <Button variant="warning">Obtener personaje</Button>
+      <main className="container text-center">
+<img src={logo} alt="logo dragon ball Z" className="mt-3" /> 
+<div>
+           {
+           mostrarSpinner ?  <Spinner className="mt-3" animation="grow" variant="warning" /> : 
+        <CardPersonaje personaje={personaje}></CardPersonaje>
+           }
+        </div>
+      
+          <Button className="my-1" variant="warning" onClick={obtenerPersonaje}>
+          Obtener Personaje
+        </Button> 
+        
       </main>
     </>
   );
--- src/componentes/CardPersonaje.jsx
+++ src/componentes/CardPersonaje.jsx
@@ -9,8 +9,7 @@ const CardPersonaje = ({personaje}) => {
       <Card.Img variant="top" src={personaje.image}/>   
       <Card.Body>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+     Descripcion: {personaje.description}
         </Card.Text>
         <ul className="list-group">
             <li className="list-group-item">Ki: {personaje.ki}</li>
